1e66519886576fe65df637c4976cc9642fc7073b
Ihap Mustapha <ihapmustapha@gmail.com>
2020-07-05 23:03:43 +0200

    feat: initiating sign-up functionality

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-var API_URL = "https://private-b2e6827-robustatask.apiary-mock.com";
-var API_PATH_SIGNUP = "/auth/register";
-var API_PATH_SIGNIN = "/auth/login";
+"use strict";
+
+document.addEventListener("DOMContentLoaded", (e) => {
+   const API_URL = "https://private-b2e6827-robustatask.apiary-mock.com";
+   const API_PATH_SIGNUP = "/auth/register";
+   const API_PATH_SIGNIN = "/auth/login";
+
+   const signUpForm = document.getElementById("signUpForm");
+
+   signUpForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const signUpFormData = new FormData(signUpForm);
+
+      fetch(`${API_URL}${API_PATH_SIGNUP}`, {
+         method: "POST",
+         body: signUpFormData,
+      })
+         .then((response) => console.log(response))
+         .catch((error) => console.log(error));
+   });
+});
