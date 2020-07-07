7316dce640d8f4dfa111d0c1683c3ad5d26901d2
Ihap Mustapha <ihapmustapha@gmail.com>
2020-07-07 23:55:52 +0200

    feat(signup form): adding validation on submit to signup form

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", (e) => {
+import { getInvalidFormInputs } from "./validation.js";
+import { showErrorForInput } from "./ui-feedback.js";
+
+document.addEventListener("DOMContentLoaded", function () {
    const API_URL = "https://private-b2e6827-robustatask.apiary-mock.com";
    const API_PATH_SIGNUP = "/auth/register";
    const API_PATH_SIGNIN = "/auth/login";
 
    const signUpForm = document.getElementById("signUpForm");
 
-   signUpForm.addEventListener("submit", (e) => {
+   signUpForm.addEventListener("submit", function (e) {
       e.preventDefault();
 
       const signUpFormData = new FormData(signUpForm);
+      const signUpFormInvalidInputs = getInvalidFormInputs(signUpFormData);
+
+      if (signUpFormInvalidInputs.length > 0) {
+         signUpFormInvalidInputs.forEach(function (invalidInputProps) {
+            showErrorForInput(invalidInputProps);
+         });
+
+         const invalidDOMElements = document
+            .getElementById("signUpForm")
+            .querySelectorAll(".is-invalid");
+
+         invalidDOMElements.forEach(function (element) {
+            element.addEventListener("input", function () {
+               this.classList.remove("is-invalid");
+            });
+         });
+         return;
+      }
 
       fetch(`${API_URL}${API_PATH_SIGNUP}`, {
          method: "POST",
          body: signUpFormData,
       })
-         .then((response) => console.log(response))
-         .catch((error) => console.log(error));
+         .then(function (response) {
+            console.log(response);
+         })
+         .catch(function (error) {
+            console.log(error);
+         });
    });
 });
