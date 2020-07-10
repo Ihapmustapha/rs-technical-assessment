013e15ddba608a0b98d3743e5c4bdbd3731b3413
Ihap Mustapha <ihapmustapha@gmail.com>
2020-07-10 02:10:42 +0200

    refactor(validation): refactoring validation functions

--- validation.js
+++ validation.js
@@ -1,69 +1,81 @@
-export function formInputIsValid(inputProps) {
-   const { type, value, name, isRequired } = inputProps;
-   const minInputLength = 8;
+export function validateInput(inputProps) {
+   const { value, name, validators } = inputProps;
+   const minInputLength = 3;
    const maxInputLength = 100;
-
-   if (isRequired && value.length === 0) return false;
 
    // regex rules
    const containsNumber = /\d/;
+   const containsSpecialChars = /[`!@#$%^&*()_+\-=[\]{};':"\\|,.<>/?~]/;
    const isEmailAddress = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
    const isStrongPassword = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/;
 
-   switch (type) {
-      case "text":
-         return (
-            (name === "fullName" ? !containsNumber.test(value) : value) &&
-            value.length <= maxInputLength &&
-            isRequired
-         );
+   let validatorsMap = {};
+   validators.forEach((validator) => (validatorsMap[validator] = validator));
 
-      case "email":
-         return isEmailAddress.test(value) && isRequired;
-
-      case "password":
-         return (
-            value.length >= minInputLength &&
-            value.length <= maxInputLength &&
-            isStrongPassword.test(value) &&
-            isRequired
-         );
-
-      default:
-         break;
+   if (validatorsMap["isRequired"] && !value) {
+      return {
+         ...inputProps,
+         isValid: false,
+         error: "this field is required",
+      };
    }
+
+   if (value.length > maxInputLength) {
+      return {
+         ...inputProps,
+         isValid: false,
+         error: `${name} is too long`,
+      };
+   }
+
+   if (value.length < minInputLength) {
+      return {
+         ...inputProps,
+         isValid: false,
+         error: `${name} is too short`,
+      };
+   }
+
+   if (
+      validatorsMap["onlyText"] &&
+      (containsNumber.test(value) || containsSpecialChars.test(value))
+   ) {
+      return {
+         ...inputProps,
+         isValid: false,
+         error: `${name} should only contain letters`,
+      };
+   }
+
+   if (validatorsMap["isEmail"] && !isEmailAddress.test(value)) {
+      return {
+         ...inputProps,
+         isValid: false,
+         error: "email address is not valid",
+      };
+   }
+
+   if (
+      validatorsMap["isStrongPassword"] &&
+      value.length < 8 &&
+      !isStrongPassword.test(value)
+   ) {
+      return {
+         ...inputProps,
+         isValid: false,
+         error:
+            "A minimum 8 characters password contains a combination of uppercase and lowercase letter, special character and number are required.",
+      };
+   }
+
+   return {
+      ...inputProps,
+      isValid: true,
+   };
 }
 
-export function getInvalidFormInputs(formData) {
-   const formInputsProps = [
-      {
-         name: "fullName",
-         value: formData.get("fullName"),
-         type: "text",
-         isRequired: true,
-      },
-      {
-         name: "email",
-         value: formData.get("email"),
-         type: "email",
-         isRequired: true,
-      },
-      {
-         name: "username",
-         value: formData.get("username"),
-         type: "text",
-         isRequired: true,
-      },
-      {
-         name: "password",
-         value: formData.get("password"),
-         type: "password",
-         isRequired: true,
-      },
-   ];
-
-   const invalidInputs = formInputsProps.filter(
-      (inputProps) => !formInputIsValid(inputProps)
-   );
-   return invalidInputs;
+export function validate(formInputs) {
+   return formInputs
+      .map((input) => validateInput(input))
+      .filter((input) => !input.isValid);
 }
